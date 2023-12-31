5b11de50838d5747b13801d6cca810b6cdb2dfac
deppfellow <raid.rafif3@gmail.com>
2023-12-31 18:55:34 +0700

    Add loop while score less than 5 for any side

--- consoleCode/script.js
+++ consoleCode/script.js
@@ -47,11 +47,43 @@ function getPlayerChoice() {
     return selection
 }
 
-let computerSelection = getComputerChoice()
-let playerSelection = getPlayerChoice()
-
 // console.log(computerSelection)
 // console.log(playerSelection)
 // process.exit()
 
-console.log(gameLogic(computerSelection, playerSelection));
+// console.log(gameLogic(computerSelection, playerSelection));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (playerScore < 5 && computerScore < 5) {
+        let computerSelection = getComputerChoice()
+        let playerSelection = getPlayerChoice()
+
+        let result = gameLogic(computerSelection, playerSelection);
+        // console.log(result)
+
+        if (result == "Player win") {
+            console.log("Player +1 point.")
+            playerScore += 1
+        } else if (result == "Comp win") {
+            console.log("Computer got a point")
+            computerScore += 1
+        } else if (result == "Tie") {
+            console.log("Tie. Nothing happen, keep going...")
+        }
+
+        console.log("Current score:")
+        console.log("Player score:", playerScore)
+        console.log("Computer score:", computerScore)
+
+    }
+
+    if (playerScore >= 5) {
+        console.log("Player has win. Game end")
+    } else if (computerScore >= 5) {
+        console.log("Computer win. Starting countdown of destruction of humanity.")
+    }
+}
+
+game()
